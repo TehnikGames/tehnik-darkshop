22eb2ff72c8787d4fe7ab8f7f4997d3fa2052d3e
TehnikGames <105563819+TehnikGames@users.noreply.github.com>
2023-02-17 21:24:46 +0100

    Add files via upload

---
+++ tehnik-blackmarket/config.lua
@@ -0,0 +1,51 @@
+Config = {}
+
+-- Blip na mapie
+Config.UseBlip = false -- najlepiej zostawic off
+Config.BlipLocation = {
+    {title="Czarny Rynek", colour=37, id=47, x = -462.73, y = -66.37, z = 44.90},
+}
+
+-- Jesli uzywasz itemku pieniędzy, dla przykladu "money" jesli uzywasz ox_inventory
+Config.CurrencyItem = 'money'
+
+-- Menu
+Config.RobberyList = {
+    [1] = {
+        bank = false,
+        Header = "Bank Fleeca",
+        icon = "fa-solid fa-building-columns",
+        minCops = 4,
+    },
+    [2] = {
+        bank = false,
+        Header = "Bank Paleto",
+        icon = "fa-solid fa-building-columns",
+        minCops = 4,
+    },
+    [3] = {
+        bank = false,
+        Header = "Bank Pacific",
+        icon = "fa-solid fa-building-columns",
+        minCops = 6
+    },
+}
+
+Config.Shop = {
+    [1] = {
+        item = "hack_usb",
+        label = "Czarny Pendrive",
+        price = 1500,
+        type = "cash",
+        icon = "fa-solid fa-laptop-code",
+    },
+
+    [2] = {
+        item = "lhseed",
+        label = "Nasiono Weed",
+        price = 1050,
+        type = "cash",
+        icon = "fa-solid fa-laptop-code",
+    }
+    
+}
